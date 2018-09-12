7ac9c2b5290f00a955fe0923d33e326938704fa8
戴黎明 <dlm@meitu.com>
2018-09-12 10:48:30 +0800

    初始化工程

--- 0x00_环境搭建/OpenGLDemo/ViewController.m
+++ 0x00_环境搭建/OpenGLDemo/ViewController.m
@@ -7,23 +7,18 @@
 //
 
 #import "ViewController.h"
+#import "OpenGLView.h"
 
 @interface ViewController ()
-
+@property (nonatomic, strong) OpenGLView *glView;
 @end
 
 @implementation ViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    self.glView = [[OpenGLView alloc] initWithFrame:CGRectMake(50, 50, 300, 300)];
+    [self.view addSubview:self.glView];
 }
-
-
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
-}
-
 
 @end
