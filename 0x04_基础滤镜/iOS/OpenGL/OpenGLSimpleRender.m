db7a357bca2f86ab75b7982d110e12b631f34096
戴黎明 <dlm@meitu.com>
2019-03-25 12:55:28 +0800

    【Feature】添加色调滤镜

--- 0x04_基础滤镜/iOS/OpenGL/OpenGLSimpleRender.m
+++ 0x04_基础滤镜/iOS/OpenGL/OpenGLSimpleRender.m
@@ -143,6 +143,53 @@ NSString *const kVignetteFragmentShaderString = SHADER_STRING
  }
  );
 
+#pragma mark - 色调
+NSString *const kHueFragmentShaderString = SHADER_STRING
+(
+ precision highp float;
+ varying highp vec2 textureCoordinate;
+ 
+ uniform sampler2D inputImageTexture;
+ uniform mediump float hueAdjust;
+ const highp  vec4  kRGBToYPrime = vec4 (0.299, 0.587, 0.114, 0.0);
+ const highp  vec4  kRGBToI     = vec4 (0.595716, -0.274453, -0.321263, 0.0);
+ const highp  vec4  kRGBToQ     = vec4 (0.211456, -0.522591, 0.31135, 0.0);
+ 
+ const highp  vec4  kYIQToR   = vec4 (1.0, 0.9563, 0.6210, 0.0);
+ const highp  vec4  kYIQToG   = vec4 (1.0, -0.2721, -0.6474, 0.0);
+ const highp  vec4  kYIQToB   = vec4 (1.0, -1.1070, 1.7046, 0.0);
+ 
+ void main () {
+     // Sample the input pixel
+     highp vec4 color   = texture2D(inputImageTexture, textureCoordinate);
+     
+     // Convert to YIQ
+     highp float   YPrime  = dot (color, kRGBToYPrime);
+     highp float   I      = dot (color, kRGBToI);
+     highp float   Q      = dot (color, kRGBToQ);
+     
+     // Calculate the hue and chroma
+     highp float   hue     = atan (Q, I);
+     highp float   chroma  = sqrt (I * I + Q * Q);
+     
+     // Make the user's adjustments
+     hue += (-hueAdjust); //why negative rotation?
+     
+     // Convert back to YIQ
+     Q = chroma * sin (hue);
+     I = chroma * cos (hue);
+     
+     // Convert back to RGB
+     highp vec4  yIQ = vec4 (YPrime, I, Q, 0.0);
+     color.r = dot (yIQ, kYIQToR);
+     color.g = dot (yIQ, kYIQToG);
+     color.b = dot (yIQ, kYIQToB);
+     
+     // Save the result
+     gl_FragColor = color;
+ }
+ );
+
 union _Marix3 {
     struct { float x, y, z; };
     struct { float r, g, b; };
@@ -159,6 +206,7 @@ typedef union _Marix3 Marix3;
 @property (nonatomic, assign) GLint contrastUniform;
 @property (nonatomic, assign) GLint luminanceUniform;
 @property (nonatomic, assign) GLint saturationUniform;
+@property (nonatomic, assign) GLint hueUniform;
 
 @property (nonatomic, assign) GLint sharpnessUniform;
 @property (nonatomic, assign) GLint imageWidthFactorUniform;
@@ -179,7 +227,7 @@ typedef union _Marix3 Marix3;
 
 - (instancetype)init {
     if (self = [super init]) {
-        self.program = [[OpenGLProgram alloc] initWithVertexShader:kVertexShaderString fragmentShader:kVignetteFragmentShaderString];
+        self.program = [[OpenGLProgram alloc] initWithVertexShader:kVertexShaderString fragmentShader:kHueFragmentShaderString];
         self.texture = [[OpenGLTexture alloc] init];
         self.percent = 1.0;
         
@@ -198,6 +246,7 @@ typedef union _Marix3 Marix3;
         self.contrastUniform = [self.program uniformIndex:@"contrast"];
         self.luminanceUniform = [self.program uniformIndex:@"rangeReduction"];
         self.saturationUniform = [self.program uniformIndex:@"saturation"];
+        self.hueUniform = [self.program uniformIndex:@"hueAdjust"];
         
         self.sharpnessUniform = [self.program uniformIndex:@"sharpness"];
         self.imageWidthFactorUniform = [self.program uniformIndex:@"imageWidthFactor"];
@@ -260,15 +309,16 @@ typedef union _Marix3 Marix3;
 //    glUniform1f(self.contrastUniform, self.percent);
 //    glUniform1f(self.luminanceUniform, self.percent);
 //    glUniform1f(self.saturationUniform, self.percent);
+    glUniform1f(self.hueUniform, self.percent);
     
 //    glUniform1f(self.sharpnessUniform, self.percent);
 //    glUniform1f(self.imageWidthFactorUniform, 1.0 / size.height);
 //    glUniform1f(self.imageHeightFactorUniform, 1.0 / size.width);
     
-    glUniform1f(self.vignetteStartUniform, self.vignetteStart);
-    glUniform1f(self.vignetteEndUniform, self.percent);
-    glUniform2f(self.vignetteCenterUniform, self.vignetteCenter.x, self.vignetteCenter.y);
-    glUniform3f(self.vignetteColorUniform, self.vignetteColor.r, self.vignetteColor.g, self.vignetteColor.b);
+//    glUniform1f(self.vignetteStartUniform, self.vignetteStart);
+//    glUniform1f(self.vignetteEndUniform, self.percent);
+//    glUniform2f(self.vignetteCenterUniform, self.vignetteCenter.x, self.vignetteCenter.y);
+//    glUniform3f(self.vignetteColorUniform, self.vignetteColor.r, self.vignetteColor.g, self.vignetteColor.b);
     
     glDrawArrays(GL_TRIANGLE_STRIP, 0, 4);
     
